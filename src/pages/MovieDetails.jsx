55452aecb067a8a5e6b5f9af6de986217696e64d
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-12-22 17:57:38 +0200

    basename

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 import { fetchMovieById } from 'services/api';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+  const navigate = useNavigate();
   useEffect(() => {
-    fetchMovieById(movieId).then(res => setMovie(res));
-  }, [movieId]);
+    fetchMovieById(movieId)
+      .then(res => setMovie(res))
+      .catch(err => {
+        console.log(err);
+        if (err.response.status === 404) {
+          navigate('/notFound');
+        }
+      });
+  }, [movieId, navigate]);
   // console.log(movie);
   if (!movie) {
     return <h1>Loading ...</h1>;
   }
+
+  const handleGoBack = () => {
+    navigate('/movies');
+  };
   return (
     <>
       <div>
+        <button onClick={handleGoBack}>Go back</button>
         <h2>{movie.title}</h2>
         <p>Rating: {movie.score}</p>
         <img src={movie.poster} alt="Movie poster" width={250} />
